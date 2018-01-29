a6b44cfb6e3bf3fa075395cf78509997a558f5c2
Olivier Leduc <account@leduc.cc>
2018-01-29 12:25:57 -0500

    Add dist

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _withHover = require('./with-hover');
+
+var _withHover2 = _interopRequireDefault(_withHover);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _withHover2.default;
